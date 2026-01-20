e1e25a74e82ab2344a32f5027f7897dbe1cea642
srojgam0907 <srojgam0907@g.educaand.es>
2026-01-20 13:58:51 +0100

    Estructura JS de eventos

---
+++ script.js
@@ -0,0 +1,37 @@
+//BOTONES
+const botonA1= document.querySelector("#btnA1");
+const botonA2= document.querySelector("#btnA2");
+const botonA3= document.querySelector("#btnA3");
+const botonA4= document.querySelector("#btnA4");
+
+//MENSAJES
+const mensajeA1= document.querySelector("#a1");
+const mensajeA2= document.querySelector("#a2");
+const mensajeA3= document.querySelector("#a3");
+const mensajeA4= document.querySelector("#a4");
+
+//EVENTOS
+
+botonA1.addEventListener("click", function(event){
+
+
+
+});
+
+botonA2.addEventListener("click", function(event){
+
+
+
+});
+
+botonA3.addEventListener("click", function(event){
+
+
+
+});
+
+botonA4.addEventListener("click", function(event){
+
+
+
+});
